662adeb97de9c4e1c67b8ca2455fe52ba87694f1
username <user@email.mail>
2020-09-16 02:29:43 +0900

    3-3(~p100 필수 프로퍼티 사용) 오타 수정

--- src/03/BooleanComponent.jsx
+++ src/03/BooleanComponent.jsx
@@ -6,7 +6,7 @@ class BooleanComponent extends Component {
     const message = this.props.bored
       ? "놀러 가자"
       : "하던 일 열심히 마무리하기";
-    return <div className="message-container">>{message}</div>;
+    return <div className="message-container">{message}</div>;
   }
 }
 
